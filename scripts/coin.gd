599b220563a051e6a4308308c9d07830447d1cd1
AflamePetal <matin.ebrahimpour81@gmail.com>
2026-09-08 21:39:06 +0330

    Background music added to the game
    Coin pickup action now has SFX

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,6 +1,13 @@
 extends Area2D
 
+@onready var pickup_sound: AudioStreamPlayer2D = $PickupSound
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var collision_shape: CollisionShape2D = $CollisionShape2D
 
 func _on_body_entered(body: Node2D) -> void:
 	print("+1 Coin!")
+	pickup_sound.play()
+	animated_sprite.visible = false
+	collision_shape.visible = false
+	await get_tree().create_timer(0.20).timeout
 	queue_free()
--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -8,9 +8,14 @@ func _on_body_entered(body: Node2D) -> void:
 	body.get_node("CollisionShape2D").queue_free()
 	
 	# Rotating the character after death
-	var tween = create_tween()
-	tween.tween_property(body, "rotation_degrees", -45.0, 0.5)\
-	.set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
+	if body.get_node("AnimatedSprite2D").flip_h == false:
+		var tween = create_tween()
+		tween.tween_property(body, "rotation_degrees", -45.0, 0.5)\
+		.set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
+	else:
+		var tween = create_tween()
+		tween.tween_property(body, "rotation_degrees", 45.0, 0.5)\
+		.set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
 	
 	timer.start()
 
